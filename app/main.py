f0a3eccba1e79dc22bbeb6a523c7ebbc5e2a8fe3
Yu-Hsuan-1220 <johnnyli122022@gmail.com>
2025-09-19 00:02:51 +0800

    commit

--- app/main.py
+++ app/main.py
@@ -5,7 +5,8 @@ from fastapi.middleware.cors import CORSMiddleware
 import os
 
 # local lib
-from app.routers import pose_check, menu, home, first_used,tuner
+
+from app.routers import pose_check, menu, home, first_used, guitar, tuner
 
 
 app = FastAPI()
@@ -28,6 +29,7 @@ app.include_router(home.router, prefix="/home", tags=["Home"])
 app.include_router(pose_check.router, prefix="/pose", tags=["Pose Check"])
 app.include_router(first_used.router, prefix="/first_used", tags=["First Used"])
 app.include_router(tuner.router,prefix="/tuner",tags=["Tuner"])
+app.include_router(guitar.router, prefix="/guitar", tags=["Guitar"])
 
 if __name__ == "__main__":
     import uvicorn
